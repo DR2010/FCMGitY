f603db9c9813bf24ca9634dffa83fa7d9e16df79
Daniel <daniellgmachado@gmail.com>
2020-11-06 08:58:06 +1100

    first commit

--- repo.cmd
+++ repo.cmd
@@ -1,13 +1,9 @@
 echo "# FCMMySQL" >> README.md
 git init
-git add README.md
+git add *
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/DR2010/FCMMySQL.git
 git push -u origin main
                 
-
-git add *
-git commit -m "2nd commit"
-git push
 
